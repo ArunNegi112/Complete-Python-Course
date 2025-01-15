7511c412a71980992395be1762f521ece3b75e40
Arun Negi <arunsinghnegi112@gmail.com>
2025-01-16 02:24:43 +0530

    Add files via upload

---
+++ 12. Logging/logs/logger.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format= '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    datefmt= '%d-%m-%Y : %H:%M:%S',
+    filename='app.log',
+    filemode='w'
+)
