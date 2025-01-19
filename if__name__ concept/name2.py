5bfb3311d8540448278a743df0fc34fedf269a53
Arun Negi <arunsinghnegi112@gmail.com>
2025-01-19 13:55:55 +0530

    Edited

--- if__name__ concept/name2.py
+++ if__name__ concept/name2.py
@@ -4,7 +4,10 @@ def greet():
 if __name__ == "__main__":
     print(greet())
 # what it does?
-# it says that, when executing, if name == main ( the original file (not imported)), only then execute this block
+# it says that, when executing, if, name == main ( the original file (not imported)), only then execute this block
 
 # Hence when importing this module in other file, greet() wont get called until explicitly called
     print(__name__)   # see that it prints "__main__"
+
+
+# Also, this line of code, acts as entry point i.e it says that code will start exectuing from here, like "int main()" in C lang.
